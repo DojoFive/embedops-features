1a144506bccbcb9c2efdd216c6c1705d72700ce9
Jimmy Gomez <jimmy.gomez@dojofive.com>
2025-03-27 14:03:27 -0400

    Add feature for yocto app development

---
+++ src/yocto-app-dev/install.sh
@@ -0,0 +1,117 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Bring in ID, ID_LIKE, VERSION_ID, VERSION_CODENAME
+. /etc/os-release
+# Get an adjusted ID independent of distro variants
+if [ "${ID}" = "debian" ] || [ "${ID_LIKE}" = "debian" ]; then
+    ADJUSTED_ID="debian"
+elif [ "${ID}" = "alpine" ]; then
+    ADJUSTED_ID="alpine"
+elif [[ "${ID}" = "rhel" || "${ID}" = "fedora" || "${ID}" = "mariner" || "${ID_LIKE}" = *"rhel"* || "${ID_LIKE}" = *"fedora"* || "${ID_LIKE}" = *"mariner"* ]]; then
+    ADJUSTED_ID="rhel"
+    VERSION_CODENAME="${ID}${VERSION_ID}"
+else
+    echo "Linux distro ${ID} not supported."
+    exit 1
+fi
+
+if type apt-get > /dev/null 2>&1; then
+    INSTALL_CMD=apt-get
+elif type apk > /dev/null 2>&1; then
+    INSTALL_CMD=apk
+elif type microdnf > /dev/null 2>&1; then
+    INSTALL_CMD=microdnf
+elif type dnf > /dev/null 2>&1; then
+    INSTALL_CMD=dnf
+elif type yum > /dev/null 2>&1; then
+    INSTALL_CMD=yum
+else
+    echo "(Error) Unable to find a supported package manager."
+    exit 1
+fi
+
+# Clean up
+clean_up() {
+    case $ADJUSTED_ID in
+        debian)
+            rm -rf /var/lib/apt/lists/*
+            ;;
+        alpine)
+            rm -rf /var/cache/apk/*
+            ;;
+        rhel)
+            rm -rf /var/cache/dnf/*
+            rm -rf /var/cache/yum/*
+            ;;
+    esac
+}
+
+pkg_mgr_update() {
+    if [ ${INSTALL_CMD} = "apt-get" ]; then
+        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+            echo "Running apt-get update..."
+            ${INSTALL_CMD} update -y
+        fi
+    elif [ ${INSTALL_CMD} = "apk" ]; then
+        if [ "$(find /var/cache/apk/* | wc -l)" = "0" ]; then
+            echo "Running apk update..."
+            ${INSTALL_CMD} update
+        fi
+    elif [ ${INSTALL_CMD} = "dnf" ] || [ ${INSTALL_CMD} = "yum" ]; then
+        if [ "$(find /var/cache/${INSTALL_CMD}/* | wc -l)" = "0" ]; then
+            echo "Running ${INSTALL_CMD} check-update ..."
+            ${INSTALL_CMD} check-update || true
+        fi
+    fi
+}
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if [ ${INSTALL_CMD} = "apt-get" ]; then
+        if ! dpkg -s "$@" > /dev/null 2>&1; then
+            pkg_mgr_update
+            ${INSTALL_CMD} -y install --no-install-recommends "$@"
+        fi
+    elif [ ${INSTALL_CMD} = "apk" ]; then
+        ${INSTALL_CMD} add \
+            --no-cache \
+            "$@"
+    elif [ ${INSTALL_CMD} = "dnf" ] || [ ${INSTALL_CMD} = "yum" ]; then
+        _num_pkgs=$(echo "$@" | tr ' ' \\012 | wc -l)
+        _num_installed=$(${INSTALL_CMD} -C list installed "$@" | sed '1,/^Installed/d' | wc -l)
+        if [ ${_num_pkgs} != ${_num_installed} ]; then
+            pkg_mgr_update
+            ${INSTALL_CMD} -y install "$@"
+        fi
+    elif [ ${INSTALL_CMD} = "microdnf" ]; then
+        ${INSTALL_CMD} -y install \
+            --refresh \
+            --best \
+            --nodocs \
+            --noplugins \
+            --setopt=install_weak_deps=0 \
+            "$@"
+    else
+        echo "Linux distro ${ID} not supported."
+        exit 1
+    fi
+}
+
+# Install required packages
+install_dependencies() {
+    echo "Installing required dependencies..."
+    check_packages tar gzip ccache git-lfs openssh-client
+}
+
+# Main function
+main() {
+    install_dependencies
+    install -m 755 yocto-setup.sh /usr/local/bin
+    install -m 755 yocto-sdk-check.sh /usr/local/bin
+    clean_up
+}
+
+# Run the main function
+main
